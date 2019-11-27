184177b6e4a83efacc103f3246de14cd0a9a2401
Pavel Karagodin <pkaragodin@illionfze.com>
2019-11-27 11:53:18 +0700

    deps: install styles components

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import styled from 'styled-components';
+
+const Container = styled.View`
+	flex: 1;
+	background-color: papayawhip;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Title = styled.Text`
+	font-size: 24px;
+	font-weight: 500;
+	color: palevioletred;
+`;
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!</Text>
-    </View>
+    <Container>
+      <Title>Open up App.tsx to start working on your app!</Title>
+    </Container>
   );
 }
 
